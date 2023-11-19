5e2c6f12eb475f25c13cde3617370b6604273b53
Rojae Wedderburn <rojaewedderburn@gmail.com>
2023-11-18 23:39:17 -0500

    Exercise 5

--- world.js
+++ world.js
@@ -1,21 +1,31 @@
 
 
-$(document).ready(function () {
-    let button = $('#lookup');
 
+$(document).ready(function () {
+    let lookForCities = false;
+    let button = $('#lookup');
+    let button_c = $('#lookup-c');
     button.click(function () {
-        getCountry($('input').val());
+        lookForCities = false;
+        getCountry($('input').val(),lookForCities);
+    });
+    button_c.click(function () {
+        lookForCities = true;
+        getCountry($('input').val(),lookForCities);
     });
 });
 
 
-function getCountry(userInput){
+function getCountry(userInput,lookForCities){
     // e.preventDefault();
     // userInput = searchField.value.toUpperCase();
     $.ajax({
         url: 'world.php',
         type: 'get',
-        data:{country: userInput},
+        data:{
+            country: userInput,
+            cityLookup: lookForCities
+        },
         success: function(data) {
             displayResults(data);
         },
--- world.php
+++ world.php
@@ -7,35 +7,62 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 $country = htmlspecialchars($_GET['country']);
+$cityLookup = $_GET['cityLookup'];
 
+if($cityLookup=='true'){
+  
+  $stmt = $conn->prepare("SELECT c.name as Name, c.district as District, c.population as Population FROM cities c join countries cs on c.country_code = cs.code WHERE cs.name = :country");
+  $stmt->bindParam(':country', $country, PDO::PARAM_STR);
+  $stmt->execute();
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo '<table class="content-table">';;
+  echo "<thead>";
+    echo "<tr>";
+      echo "<th>NAME</th>";
+      echo "<th>District</th>";
+      echo "<th>Population</th>";
+    echo "</tr>";
+  echo "</thead>";
+  echo "<tbody>";
+  foreach ($results as $row){
+    echo "<tr>";
+      echo "<td>".$row['Name']."</td>";
+      echo "<td>".$row['District']."</td>";
+      echo "<td>".$row['Population']."</td>";
+    echo "</tr>";
+  }
+  echo "</tbody>";
+echo "</table>";
+}else{
+  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $stmt->execute();
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo '<table class="content-table">';;
+  echo "<thead>";
+    echo "<tr>";
+      echo "<th>NAME</th>";
+      echo "<th>CONTINENT</th>";
+      echo "<th>INDEPENDENCE</th>";
+      echo "<th>HEAD OF STATE</th>";
+    echo "</tr>";
+  echo "</thead>";
+  echo "<tbody>";
+  foreach ($results as $row){
+    echo "<tr>";
+      echo "<td>".$row['name']."</td>";
+      echo "<td>".$row['continent']."</td>";
+      echo "<td>".$row['independence_year']."</td>";
+      echo "<td>".$row['head_of_state']."</td>";
+    echo "</tr>";
+  }
+  echo "</tbody>";
+echo "</table>";
+  
+}
 
-$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // Use a prepared statement to prevent SQL injection
 
 ?>
 
-<table class="content-table">
-  <thead>
-    <tr>
-      <th>NAME</th>
-      <th>CONTINENT</th>
-      <th>INDEPENDENCE</th>
-      <th>HEAD OF STATE</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($results as $row): ?>
-    <tr>
-      <td><?= $row['name']; ?></td>
-      <td><?= $row['continent']; ?></td>
-      <td><?= $row['independence_year']; ?></td>
-      <td><?= $row['head_of_state']; ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
 					  
 
 
